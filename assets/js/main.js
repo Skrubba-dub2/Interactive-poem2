14d98c38c239021d855c3fdcee3b15240b3852c4
Skrubba-dub2 <kta220001@utdallas.edu>
2026-07-20 23:30:03 +0000

    updated java script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 // main.js
 
-console.log("Hello, World!");
+// Define Random Number Function
+function RandomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Define divs variable as all divs inside grid containter
+let divs = document.querySelectorAll(".grid-container div");
