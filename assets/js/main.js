54fd005bc14ff9647d2a7ff707d009197542cd6e
Skrubba-dub2 <kta220001@utdallas.edu>
2026-07-21 21:57:29 +0000

    updated title

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,5 @@
 // main.js
 
-// Define Random Number Function
-function randomNumber(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-//Define divs variable as all divs inside grid containter
-let divs = document.querySelectorAll(".grid-container div");
-
-// Define Randomized function
-function randomize() {
-    console.log(divs.length, "randomize!");
-
-    // just the grid divs
-    divs.forEach(function(div) {
-        let scale = randomNumber(.5, 5.2);
-        let translatex = randomNumber(0, 50);
-        let translateY = randomNumber(0, 20);
-        let rotate = randomNumber(0, 360);
-
-        // 50% of the time...
-        if (Math.random() > 0.5) {
-            scale = 1; // don't affect scale
-            translateX = randomNumber(0, 80);
-            translateY = randomNumber(0, 80);
-        }
-
-        div.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)`;
-    })
-}
-
 const textContainer = document.getElementById('orbitText');
 const text = textContainer.innerText;
 
